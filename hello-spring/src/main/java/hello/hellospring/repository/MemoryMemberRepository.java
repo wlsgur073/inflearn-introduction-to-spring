e3f5986a6afbc126de46f2d14790c79aa79f3bfb
wlsgur073 <wlsgur073@gmail.com>
2023-11-20 23:51:50 +0900

    회원 도메인과 리포지토리 만들기

---
+++ hello-spring/src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -0,0 +1,38 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.*;
+
+public class MemoryMemberRepository implements MemberRepository{
+
+    private static Map<Long, Member> store = new HashMap<>();
+    private static Long sequence = 0L;
+
+    @Override
+    public Member save(Member member) {
+        member.setId(++sequence);
+        store.put(member.getId(), member);
+        return member;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        //  null 이 반환될 가능성이 있으면 Optional 로 감싸서 반환한다.
+        return Optional.ofNullable(store.get(id));
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        //  store 에서 filter 의 값이 true 인 것이 하나라도 찾게 되면 return, 없다면 null 반환
+        return store.values().stream()
+                .filter(member -> member.getName().equals(name))
+                .findAny();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        //  store.values()는 member 이다.
+        return new ArrayList<>(store.values());
+    }
+}
